bcffffdf38c198b39cf28de45e0cbc8d556739fd
JotroopS <jotroopsingh644@gmail.com>
2023-10-21 11:27:34 +0200

    Calcolo della classe, del CDIR e della subnet mask di default

--- Calcolatore/Form1.Designer.cs
+++ Calcolatore/Form1.Designer.cs
@@ -33,7 +33,14 @@
             this.NReti = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.Calcola = new System.Windows.Forms.Button();
-            this.Tabella = new System.Windows.Forms.ListBox();
+            this.Classe = new System.Windows.Forms.TextBox();
+            this.CDIR = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SubnetDef = new System.Windows.Forms.TextBox();
+            this.Subnet = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // NHost
@@ -81,20 +88,87 @@
             this.Calcola.UseVisualStyleBackColor = true;
             this.Calcola.Click += new System.EventHandler(this.Calcola_Click);
             // 
-            // Tabella
+            // Classe
             // 
-            this.Tabella.FormattingEnabled = true;
-            this.Tabella.Location = new System.Drawing.Point(253, 19);
-            this.Tabella.Name = "Tabella";
-            this.Tabella.Size = new System.Drawing.Size(528, 420);
-            this.Tabella.TabIndex = 5;
+            this.Classe.Location = new System.Drawing.Point(295, 58);
+            this.Classe.Name = "Classe";
+            this.Classe.Size = new System.Drawing.Size(52, 20);
+            this.Classe.TabIndex = 5;
+            // 
+            // CDIR
+            // 
+            this.CDIR.Location = new System.Drawing.Point(295, 100);
+            this.CDIR.Name = "CDIR";
+            this.CDIR.Size = new System.Drawing.Size(40, 20);
+            this.CDIR.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(292, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Classe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(292, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "CDIR";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(292, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Subnet Mask di default";
+            // 
+            // SubnetDef
+            // 
+            this.SubnetDef.Location = new System.Drawing.Point(295, 142);
+            this.SubnetDef.Name = "SubnetDef";
+            this.SubnetDef.Size = new System.Drawing.Size(161, 20);
+            this.SubnetDef.TabIndex = 10;
+            // 
+            // Subnet
+            // 
+            this.Subnet.Location = new System.Drawing.Point(295, 184);
+            this.Subnet.Name = "Subnet";
+            this.Subnet.Size = new System.Drawing.Size(175, 20);
+            this.Subnet.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(292, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(178, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Subnet Mask di sottorete";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Tabella);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Subnet);
+            this.Controls.Add(this.SubnetDef);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CDIR);
+            this.Controls.Add(this.Classe);
             this.Controls.Add(this.Calcola);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.NReti);
@@ -115,7 +189,14 @@
         private System.Windows.Forms.TextBox NReti;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button Calcola;
-        private System.Windows.Forms.ListBox Tabella;
+        private System.Windows.Forms.TextBox Classe;
+        private System.Windows.Forms.TextBox CDIR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox SubnetDef;
+        private System.Windows.Forms.TextBox Subnet;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Calcolatore/Form1.cs
+++ Calcolatore/Form1.cs
@@ -13,6 +13,7 @@ namespace Calcolatore
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
@@ -22,26 +23,39 @@ namespace Calcolatore
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        {
-            
-        };
-    }
+        }
 
         private void Calcola_Click(object sender, EventArgs e)
         {
-            int nhost = int.Parse(NHost.Text);
-            if (nhost <= 254)
+            int nhost = int.Parse(NHost.Text), nreti = int.Parse(NReti.Text); int Cdir = Convert.ToInt32(Math.Log(nhost + 2, 2) + Math.Log(nreti, 2));
+            if (Cdir <= 8)
             {
-
+                Classe.Text = "Classe C";
+                CDIR.Text = "/8";
+                SubnetDef.Text = "255.255.255.0";
             }
-            else if (nhost > 254 &&  nhost <= 65534)
+            else if (Cdir <= 16)
             {
-
+                Classe.Text = "Classe B";
+                CDIR.Text = "/16";
+                SubnetDef.Text = "255.255.0.0";
             }
-            else if (nhost > 65534 && nhost <= 16777214)
+            else if (Cdir <= 24)
             {
-
+                Classe.Text = "Classe A";
+                CDIR.Text = "/24";
+                SubnetDef.Text = "255.0.0.0";
             }
+            else if (Cdir > 24)
+            {
+                MessageBox.Show("Hai inserito troppi host");
+                NHost.Clear();
+                NReti.Clear();
+                Classe.Clear();
+                CDIR.Clear();
+                SubnetDef.Clear();
+            }
+
         }
     }
 }
